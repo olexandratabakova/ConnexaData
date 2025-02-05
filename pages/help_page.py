ef20eb65acf055ac11dd74e8cd30c050a7f29559
olexandratabakova <talexkpi6@gmail.com>
2025-02-05 22:53:27 +0200

    k

--- pages/help_page.py
+++ pages/help_page.py
@@ -1,43 +1,78 @@
 from dash import html
-from styles import common_styles, h1_style, h2_style, description_style, ul_style, button_style_backtohome
+from styles import common_styles, h1_style, h2_style, description_style, button_style_backtohome
 
 layout = html.Div(
-    style={**common_styles, 'height': '100vh'},
+    style={**common_styles, 'height': '100vh', 'display': 'flex', 'justifyContent': 'center', 'alignItems': 'center'},
     children=[
-        html.H1("ConnexaData - Help", style=h1_style),
         html.Div(
-            "This is the help page. Below are some instructions and definitions to help you navigate and understand the features of ConnexaData.",
-            style=description_style
-        ),
-        html.Div(
-            [
-                html.H2("Glossary", style=h2_style),
-                html.Ul(
-                    children=[
-                        html.Li([
-                            html.B("ConnexaData: "),
-                            "a service for visualizing tables that allows users to identify and analyze connections within data using clusters and graphs."
-                        ]),
-                        html.Li([
-                            html.B("Graph: "),
-                            "a data structure consisting of nodes (vertices) and the connections between them (edges). Used to visualize relationships between objects within the data."
-                        ]),
-                        html.Li([
-                            html.B("Cluster: "),
-                            "a group of interrelated objects or nodes in a graph. Clusters represent tightly connected groups of data, making it easier to understand the structure and interactions within the table."
-                        ]),
-                        html.Li([
-                            html.B("\"Table\" Tab: "),
-                            "a section that contains the uploaded table, where data is presented in rows and columns for easy viewing and preliminary analysis."
-                        ]),
-                    ],
-                    style=ul_style
+            style={'width': '60%', 'maxWidth': '700px', 'paddingLeft': '40px', 'paddingRight': '40px'},  # Smaller width
+            children=[
+                html.H1("ConnexaData - Help", style=h1_style),
+                html.Div(
+                    "This is the help page. Below are some instructions and definitions to help you navigate and understand the features of ConnexaData.",
+                    style={**description_style, 'textAlign': 'left'}
                 ),
+
+                html.H2("Document", style={**h2_style, 'textAlign': 'left'}),
+                html.P(
+                    children=[
+                        "You can upload your document in ",
+                        html.I("txt"), ", ",
+                        html.I("pdf"), ", or ",
+                        html.I("docx"),
+                        " format and analyse it to determine the influence of related people or objects. "
+                        "The percentage displayed shows how much of the document has been analysed. You can stop the process at any time, and the analysis completed up to that point will be saved."
+                    ],
+                    style={**description_style, 'textAlign': 'left'}
+                ),
+
+                html.H2("Table", style={**h2_style, 'textAlign': 'left'}),
+                html.P(
+                    children=[
+                        "Here, you can view the intermediate results of the analysis between your text and the visualisation. On this page, you can also access the ",
+                        html.B("\"Table Influence\""),
+                        " section, where you can see the degree of influence for each object."
+                    ],
+                    style={**description_style, 'textAlign': 'left'}
+                ),
+                html.P(
+                    children=[
+                        "The ",
+                        html.B("Object Degree"),
+                        " is calculated as the degree of each ",
+                        html.I("node"),
+                        " in the graph, reflecting how much influence an object or person has within the dataset."
+                    ],
+                    style={**description_style, 'textAlign': 'left'}
+                ),
+
+                html.H2("Statistics", style={**h2_style, 'textAlign': 'left'}),
+                html.P(
+                    children=[
+                        "This section provides insights into the frequency of mentions of specific ",
+                        html.I("objects"),
+                        " within your document. It also shows the influence these objects hold within the visualisation."
+                    ],
+                    style={**description_style, 'textAlign': 'left'}
+                ),
+
+                html.H2("Visualisation", style={**h2_style, 'textAlign': 'left'}),
+                html.P(
+                    children=[
+                        "The ",
+                        html.B("semantic maps"),
+                        " visualise the relationships between the entities in your document. You can adjust the parameters for better viewing, such as choosing the maximum number of ",
+                        html.I("objects"),
+                        " to display. This helps with readability and focuses the view on the most important data."
+                    ],
+                    style={**description_style, 'textAlign': 'left'}
+                ),
+
+                html.Div(
+                    html.A("Back to Home", href="/", style=button_style_backtohome),
+                    style={'textAlign': 'center', 'marginTop': '20px'}
+                )
             ]
-        ),
-        html.Div(
-            html.A("Back to Home", href="/", style=button_style_backtohome),
-            style={'textAlign': 'center', 'marginTop': '20px'}
         )
     ]
 )
